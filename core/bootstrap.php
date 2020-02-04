34eff6bbfff4aa6b84494cc9a71a884e19a6e5a7
Hazem Khalil <hazem.ibrahim.khalil@gmail.com>
2020-02-04 07:03:19 +0200

    Using Composer For Autoloading

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,13 +4,6 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require "core/Router.php";
-require "core/Request.php";
-require "core/database/QueryBuilder.php";
-require "core/database/Connection.php";
-
-$config = require 'config.php';
-
 $app['database'] = new QueryBuilder(
     Connection::make($app['config']['database']),
 );
